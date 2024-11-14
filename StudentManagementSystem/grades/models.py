97122f4455324abcd22ada9495997366cb5d1914
tomerismeshitbai <tomerismesitbaj@gmail.com>
2024-11-14 23:37:47 +0500

    creating models

--- StudentManagementSystem/grades/models.py
+++ StudentManagementSystem/grades/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from students.models import Student
+from courses.models import Course
+from users.models import User
 
-# Create your models here.
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=2)
+    date = models.DateField(auto_now_add=True)
+    teacher = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, limit_choices_to={'role': 'teacher'})
+
+    def __str__(self):
+        return f"{self.student.user.username} - {self.course.name}: {self.grade}"
