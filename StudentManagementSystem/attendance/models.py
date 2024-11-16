4803866fbd2186604c6d90c0c2e4fab72cd4cea2
tomerismeshitbai <tomerismesitbaj@gmail.com>
2024-11-16 23:06:58 +0500

    Creating Models and Relationships

--- StudentManagementSystem/attendance/models.py
+++ StudentManagementSystem/attendance/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from students.models import Student
+from courses.models import Course
 
-# Create your models here.
+class Attendance(models.Model):
+    STATUS_CHOICES = [
+        ('present', 'Present'),
+        ('absent', 'Absent'),
+        ('late', 'Late')
+    ]
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date = models.DateField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.student.user.username} - {self.course.name}: {self.status}"
--- StudentManagementSystem/students/models.py
+++ StudentManagementSystem/students/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
+    dob = models.DateField(null=True, blank=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.user.email}"
